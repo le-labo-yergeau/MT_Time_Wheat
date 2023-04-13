379de17532b8b9bb825eff7173a02505455d2b32
yergeaue <etienne.yergeau@inrs.ca>
2023-04-13 15:32:47 -0400

    added pheatmap package

--- src/01-LoadPackages.R
+++ src/01-LoadPackages.R
@@ -1,2 +1,2 @@
 install.packages("pacman")
-pacman::p_load(pacman, EBSeq, UpSetR, ggpubr, tidyverse, RColorBrewer, here, data.table, vegan)
+pacman::p_load(pacman, EBSeq, UpSetR, ggpubr, tidyverse, RColorBrewer, here, data.table, vegan, grid, pheatmap)
