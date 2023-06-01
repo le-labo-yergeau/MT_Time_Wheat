308629d76685b0e7dbe44d45199358a2b8c73b36
yergeaue <etienne.yergeau@inrs.ca>
2023-06-01 11:50:10 -0400

    Cleaned up folder

--- src/DE-PC-55475.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --account=def-yergeaue-ab
-#SBATCH --mem-per-cpu=64G
-#SBATCH --time=0-24:00
-module load gcc/9.3.0 r/4.0.2
-Rscript 06-DE.R
--- src/DE.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --account=def-yergeaue-ab
-#SBATCH --mem-per-cpu=32G
-#SBATCH --time=0-12:00
-module load gcc/9.3.0 r/4.0.2
-Rscript 06-DE.R
